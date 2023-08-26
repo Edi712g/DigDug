82c54909e3d98256022d330e1b3e341a5fd78fc0
EDUARDO MARTINEZ COLLAZO <143230456+Edi712g@users.noreply.github.com>
2023-08-26 00:12:12 +0000

    codigo inicial main

---
+++ src/main.cpp
@@ -0,0 +1,15 @@
+
+
+int main(int argc, char const *argv[])
+{
+    for (int (size_t i = 0; i < count; i++)
+    {
+        /* code */
+    }
+     i = 0; i < count; i++)
+    {
+        /* code */
+    }
+    
+    return 0;
+}
