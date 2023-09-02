655c7d7d9f8084744d92bc76d8de7f012aea6cf6
EDUARDO MARTINEZ COLLAZO <143230456+Edi712g@users.noreply.github.com>
2023-09-02 01:05:16 +0000

    clase de viernes-clase,chmod. -I

---
+++ include/Mascota.hpp
@@ -0,0 +1,25 @@
+#include <string>
+#include <iostream>
+
+class Mascota
+{
+private:
+    int vida;
+    std::string nombre;
+
+public:
+    Mascota()
+    {
+        this-> vida = 100;
+    }
+
+    void Establecernombre (std::string nombre) 
+    {
+        this-> nombre = nombre;
+    }
+    ~Mascota() {}
+    void DecirNombre()
+    {
+        std::cout<<"Mi nombre es: "<< this->nombre << std:: endl;
+    }
+};
--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,24 @@
 #include <iostream>
+#include <iostream>
+#include <Mascota.hpp>
+
 
 
 int main(int argc, char const *argv[])
 {
     std::cout<<"mensaje"<<std::endl;
-    for (int i = 0; i < 10; i++)
-    {
-        /* code */
-    }
 
+        Mascota m1,m2,m3; //se crean tres instancias
+        
+        m1.Establecernombre("Firulais");
+        m2.Establecernombre("Pedro");
+        m3.Establecernombre("Juan");
+    
+    m1.DecirNombre();
+    m2.DecirNombre();
+    m3.DecirNombre();
     return 0;
+    //-I inclucion en el directorio 
+    //chmod u+x compilar.sh permiso para compilar cambiar el modo de usuario + ejecutable
+
 }
