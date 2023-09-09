c48f347ba96deeed9b90e174a88df50474132aaa
EDUARDO MARTINEZ COLLAZO <143230456+Edi712g@users.noreply.github.com>
2023-09-09 00:43:39 +0000

    pree inicio de clases y reed de digdug.clase vrn

---
+++ include/DigDug.hpp
@@ -0,0 +1,9 @@
+#pragma once
+class DigDug
+{
+private:
+    /* data */
+public:
+    DigDug(/* args */) {}
+    ~DigDug() {}
+};
--- readme.md
+++ readme.md
@@ -1,1 +1,3 @@
 # DigDug
+desctribcion de mi asobroso proyecto
+![imagen logo](/images/800px-Dig_Dug-Logo-o.svg.png)
